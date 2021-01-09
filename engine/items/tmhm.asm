816c939acf88470b7d55f18350119922d2c2017e
Laulajatar <git@laulajatar.de>
2021-01-09 15:08:49 +0100

    reusable tms, laura player, normal gen2 type matchup

--- engine/items/tmhm.asm
+++ engine/items/tmhm.asm
@@ -151,7 +151,6 @@ TeachTMHM:
 
 	ld c, HAPPINESS_LEARNMOVE
 	callfar ChangeHappiness
-	call ConsumeTM
 	jr .learned_move
 
 .nope
@@ -512,27 +511,6 @@ Function2c8e4: ; unreferenced
 	cp NUM_TMS * 2
 	ret nc
 	ld [hl], a
-	ret
-
-ConsumeTM:
-	call ConvertCurItemIntoCurTMHM
-	ld a, [wTempTMHM]
-	dec a
-	ld hl, wTMsHMs
-	ld b, 0
-	ld c, a
-	add hl, bc
-	ld a, [hl]
-	and a
-	ret z
-	dec a
-	ld [hl], a
-	ret nz
-	ld a, [wTMHMPocketScrollPosition]
-	and a
-	ret z
-	dec a
-	ld [wTMHMPocketScrollPosition], a
 	ret
 
 CountTMsHMs:
